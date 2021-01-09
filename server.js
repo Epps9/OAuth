83bc7a03fefdd8cd53e8f4b71ab4aeebe2c5ecab
Ewa Witkowska <ewa.witkowska09@gmail.com>
2021-01-09 18:26:48 +0100

    Add logging through google -> success site displays

--- server.js
+++ server.js
@@ -8,18 +8,6 @@ const session = require('express-session');
 
 const app = express();
 
-passport.use(new GoogleStrategy({
-  clientID: '803743038529-qc7q3ccmvbcjv1fs2g3khbq94umuh935.apps.googleusercontent.com',
-  clientSecret: '-AoR8fgJqJz517Qp5akZm2Ej',
-  callbackURL: 'http://localhost:8000/auth/google/callback'
-}, (accessToken, refreshToken, profile, done) => {
-done(null, profile);
-}));
-
-app.use(session({ secret: '12345' }));
-app.use(passport.initialize());
-app.use(passport.session());
-
 app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' }));
 app.set('view engine', '.hbs');
 
@@ -28,6 +16,26 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '/public')));
 
+app.use(session({ secret: '12345' }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new GoogleStrategy({
+  clientID: '803743038529-qc7q3ccmvbcjv1fs2g3khbq94umuh935.apps.googleusercontent.com',
+  clientSecret: '-AoR8fgJqJz517Qp5akZm2Ej',
+  callbackURL: 'http://localhost:8000/auth/google/callback'
+}, (accessToken, refreshToken, profile, done) => {
+done(null, profile);
+}));
+
+passport.serializeUser((user, serialize) => {
+  serialize(null, user);
+});
+
+// deserialize user when reading from session
+passport.deserializeUser((obj, deserialize) => {
+  deserialize(null, obj);
+});
 
 app.get('/', (req, res) => {
   res.render('index');
@@ -41,8 +49,18 @@ app.get('/user/no-permission', (req, res) => {
   res.render('noPermission');
 });
 
-app.use('/', (req, res) => {
-  res.status(404).render('notFound');
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['email', 'profile'] }));
+
+app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/user/no-permission' }),
+(req, res) => {
+  res.redirect('/user/logged');
+}
+);
+
+
+app.get('/auth/google/callback', (req, res) => {
+  res.send(`I'm back from Google!`);
 });
 
 app.listen('8000', () => {
